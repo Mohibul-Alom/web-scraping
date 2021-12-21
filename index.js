193d21d2b4bbac2e6753f7105dd25b0525ceae81
Mohibul-alom <mohibulalom@outlook.es>
2021-12-21 21:14:38 +0100

    FEAT: refactored code

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const { chromium } = require('playwright');
 
-(async () => {
-    try {
-
-        const browser = await chromium.launch();
-        const page = await browser.newPage();
-        await page.goto("https://www.shopdutyfree.es/comprar-oferta/wifi-m1-128-gb-plata-barato.html");
-        const content = await page.textContent('[id="product-availability"]')
-        const hasStock = content === ' ' ? true : false;
-        console.log({ content: content, hasStock: hasStock });
-        await browser.close();
-    } catch (error) {
-        console.error(error);
+const shops = [
+    {
+        vendor: 'Shopdutyfree',
+        url: "https://www.shopdutyfree.es/comprar-oferta/wifi-m1-128-gb-plata-barato.html",
+        checkStock: async ({ browser, url }) => {
+            const page = await browser.newPage();
+            await page.goto(url);
+            const content = await page.textContent('[id="product-availability"]');
+            const hasStock = content === ' ' ? true : false;
+            return hasStock;
+        }
     }
+];
+
+(async function () {
+    const browser = await chromium.launch();
+    for (const shop of shops) {
+        const { checkStock, vendor, url } = shop;
+        const hasStock = await checkStock({ browser, url });
+        console.log(`${vendor}: ${hasStock ? "Has Stock 🤪" : "Out of Stock 😥"}`);
+    }
+    await browser.close();
 })()
-
-
